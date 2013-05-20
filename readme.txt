411a6bf6b8343fc93a03252006e025131d1cc03c
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-20 10:27:17 +0000

    version 2.9
    git-svn-id: https://plugins.svn.wordpress.org/social-master/tags/2.9@715541 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,143 @@
+=== Social Master ===
+Contributors: Techgasp
+Tags: wordpress, facebook, twitter, google, instagram, pinterest, youtube, yahoo, social, share, like, tweet
+Requires at least: 3.5
+Tested up to: 3.5.1
+Stable tag: 2.9
+License: GPLv2 or later
+
+Social Master is a light weight and shiny clean code wordpress plugin WIDGET that you need to boost your wordpress social engagement.
+
+
+== Description ==
+
+Social Master is a light weight and shiny clean code wordpress plugin WIDGET that you need to boost your wordpress social engagement, attracting new users, visits or sales. Social Master replaces a bunch of extensions keeping your wordpress website code clean and with fast page loading times because it makes NO use of Javascipt or Ajax.
+
+Buil-in html5 and iframe, Social Master combines all major social networks sharing tools, Facebook Like and Send, Twitter Follow and Re-Tweet, Google + plus, LinkedIn Share, Pinterest "pin it" Share, View on Instagram, Youtube Subscribe, StumbleUpon Share, MySpace Share, Digg Share and Reddit Share, with more social networks coming in future updates.
+
+The plugin is packed with many display options, in fact you can decide what buttons to show in the frontend and also the bubble per button that displays the number of social sharing. It will give your website or blog the needed social sharing viral boost while making your content look like a professional.
+
+**Social Master is the Wordpress social sharing Supreme widget**
+
+**LITE Version Contains:**
+
+* Facebook Like Button
+* Twitter Follow Button
+* Tweet Button
+* Google Plus Button
+
+**ADVANCED Version Contains:**
+
+[Advanced Version Link](http://wordpress.techgasp.com/social-master/)
+
+* Facebook Like Button
+* Twitter Follow Button
+* Tweet Button
+* Google + Plus Button
+* LinkedIn Button
+* Pinterest "pin it" Button
+* View on Instagram Button
+* Youtube Subscribe Button
+* StumbleUpon Button
+* MySpace Button
+* Digg Button
+* Reddit Button
+
+
+== Installation ==
+
+1. Upload everything into the "/wp-content/plugins/" directory of your WordPress site.
+2. Activate in the "Plugins" admin panel.
+3. Visit the "Widget Page | Social Master" to configure social buttons providers and your template widget position.
+
+
+== Frequently Asked Questions ==
+
+= Issues with buttons display =
+Please update to latest version. Up-to-date API's are available
+
+= WHERE ARE ALL THE BUTTONS? =
+LITE Version Contains Facebook Like, Twitter Follow, Tweet and Google Plus
+ADVANCED Version constains all Buttons and Social Networks. Facebook Like, Twitter Follow and Re-Tweet, Google + plus, LinkedIn Share, Pinterest "pin it" Share, View on Instagram, Youtube Subscribe, StumbleUpon Share, MySpace Share, Digg Share, Reddit Share.
+ADVANCED Version also contains shortcode framework to use the widget inside your pages and posts.
+* [Advaced Version Link](http://wordpress.techgasp.com/download/social-sharing/social-master-for-wordpress/)
+
+= Need More Help =
+
+* [ABOUT DOCUMENTATION](http://worpress.techgasp.com)
+* [ABOUT INSTALLATION](http://worpress.techgasp.com)
+* [SUPPORT](http://worpress.techgasp.com/support)
+
+
+== Screenshots ==
+
+1. Widget With Facebook FanPage Total Count
+2. Widget With Facebook Like Your Individual Website Pages
+3. Advanced Widget with all Social Network Buttons
+4. Advanced Widget inside pages and posts with shortcode framework
+
+
+== Changelog ==
+
+= 2.9 =
+* Adding shortcode framework only available in advanced version
+* Adding more screenshots
+
+= 2.8 =
+* Fixing User Array Warnings
+* Fixing Widget Undefined Index
+
+= 2.7 =
+* Fixing Warning
+* Fixing a few typos
+
+= 2.6 =
+* Adding more Buttons
+* Adding 1 more screenshot
+* Updating plugin admin page
+* Updating plugin widget page
+
+= 2.5 =
+* Adding Facebook double punch option. Like your Facebook Fan Page or Like Your Individual Website Page.
+* Admin Logos
+
+= 2.4 =
+* Fixing svn and name space issues
+* Sorry, first plugin
+
+= 2.3 =
+* Adding twitter bubble
+
+= 2.2 =
+* Updating admin page
+
+= 2.1 =
+* First Stable Release
+* Adding admin page
+
+= 2.0 =
+* First Stable Release
+* Includes final widget code
+* Adding admin page
+
+= 1.5 =
+* Major Improvents and fixes
+* Also adding google plus button
+
+= 1.0 =
+* First Beta Release
+
+
+== Upgrade Notice ==
+
+= 2.5 =
+* IMPORTANT Adding Facebook Double Puch Action
+
+= 2.4 =
+* IMPORTANT Fixing Name Space Issues
+
+= 2.0 =
+* IMPORTANT Implemented a better framework
+
+= 1.0 =
+* First Release ;)
---
+++ techgasp-socialmaster-widget.php
@@ -0,0 +1,132 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'techgasp_socialmaster_widget' );
+//Register Widget
+function techgasp_socialmaster_widget() {
+register_widget( 'techgasp_socialmaster_widget' );
+}
+
+class techgasp_socialmaster_widget extends WP_Widget {
+	function techgasp_socialmaster_widget() {
+	$widget_ops = array( 'classname' => 'Social Master', 'description' => __('Social Master is a light weight and shiny clean code wordpress plugin that you need to boost your wordpress social engagement, attracting new users, visits or sales. ', 'Social Master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'techgasp_socialmaster_widget' );
+	$this->WP_Widget( 'techgasp_socialmaster_widget', __('Social Master', 'social master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		//Our variables from the widget settings.
+		$title = "Social Master";
+		@$uribase = site_url( $path, $scheme );
+		@$uricurrent = get_page_uri( $page_id );
+		$socialspacer ="'";
+		$show_facebook = isset( $instance['show_facebook'] ) ? $instance['show_facebook'] :false;
+		$facebook_display = isset( $instance['facebook_display'] ) ? $instance['facebook_display'] :false;
+		$facebook_page = $instance['facebook_page'];
+		$facebook_id = $instance['facebook_id'];
+		$show_twitter = isset( $instance['show_twitter'] ) ? $instance['show_twitter'] :false;
+		$twitter_user = $instance['twitter_user'];
+		$show_google = isset( $instance['show_google'] ) ? $instance['show_google'] :false;
+		echo $before_widget;
+		
+		// Display the widget title
+	if ( $title )
+		echo $before_title . $title . $after_title;
+	//Display Facebook
+	if ( $show_facebook ){
+		if ( $facebook_display ){
+		echo '<center><div class="fb-like" data-href="'.$facebook_page.'" data-send="false" data-layout="box_count" data-width="90" data-show-faces="false" style="overflow: hidden !important;"></div>' .
+                        '<div id="fb-root"></div>' .
+                        '<script>(function(d, s, id) {' .
+                        'var js, fjs = d.getElementsByTagName(s)[0];' .
+                        'if (d.getElementById(id)) return;' .
+                        'js = d.createElement(s); js.id = id;' .
+                        'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+                        'fjs.parentNode.insertBefore(js, fjs);' .
+                        '}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script></center>';
+		}
+		else {
+		echo '<center><div class="fb-like" data-href="'.$uribase.'/'.$uricurrent.'" data-send="false" data-layout="box_count" data-width="90" data-show-faces="false" style="overflow: hidden !important;"></div>' .
+			'<div id="fb-root"></div>' .
+			'<script>(function(d, s, id) {' .
+			'var js, fjs = d.getElementsByTagName(s)[0];' .
+			'if (d.getElementById(id)) return;' .
+			'js = d.createElement(s); js.id = id;' .
+			'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+			'fjs.parentNode.insertBefore(js, fjs);' .
+			'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script></center>';
+		}
+	}
+	//Display Twitter
+	if ( $show_twitter )
+			echo '<center><iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://mikeseese.com/tw_vert_follow.php?username='.$twitter_user.'" style="height: 62px; width: 60px;"></iframe></br>'.
+			'<a href="https://twitter.com/share" class="twitter-share-button" data-url="'.$uribase.'.'.$uricurrent.'" data-via="'.$twitter_user.'" data-count="none">Tweet</a>' .
+			'<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'.$socialspacer.'http'.$socialspacer.':'.$socialspacer.'https'.$socialspacer.';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'.$socialspacer.'://platform.twitter.com/widgets.js'.$socialspacer.';fjs.parentNode.insertBefore(js,fjs);}}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'twitter-wjs'.$socialspacer.');</script>';
+	//Display Google Plus
+	if ( $show_google )
+			echo '<center><div class="g-plusone" data-size="tall"></div>' .
+			'<script type="text/javascript">' .
+			'(function() {' .
+			'var po = document.createElement('.$socialspacer.'script'.$socialspacer.'); po.type = '.$socialspacer.'text/javascript'.$socialspacer.'; po.async = true;' .
+			'po.src = '.$socialspacer.'https://apis.google.com/js/plusone.js'.$socialspacer.';' .
+			'var s = document.getElementsByTagName('.$socialspacer.'script'.$socialspacer.')[0]; s.parentNode.insertBefore(po, s);' .
+			'})();' .
+			'</script></center>';
+		
+		echo $after_widget;
+	}
+	//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['show_facebook'] = $new_instance['show_facebook'];
+		$instance['facebook_display'] = $new_instance['facebook_display'];
+		$instance['facebook_page'] = strip_tags( $new_instance['facebook_page'] );
+		$instance['facebook_id'] = strip_tags( $new_instance['facebook_id'] );
+		$instance['show_twitter'] = $new_instance['show_twitter'];
+		$instance['twitter_user'] = $new_instance['twitter_user'];
+		$instance['show_google'] = $new_instance['show_google'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'title' => __('Social Master', 'social master'), 'show_facebook' => false, 'facebook_display' => false, 'facebook_page' => false, 'facebook_id' => false, 'show_twitter' => false, 'twitter_user' => false, 'show_google' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<b>Check the buttons to be displayed:</b>
+	<p>
+	<input type="checkbox" <?php checked( (bool) $instance['show_facebook'], true ); ?> id="<?php echo $this->get_field_id( 'show_facebook' ); ?>" name="<?php echo $this->get_field_name( 'show_facebook' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_facebook' ); ?>"><b><?php _e('Facebook Button', 'social master'); ?></b></label></br>
+	</p>
+	<input type="checkbox" <?php checked( (bool) $instance['facebook_display'], true ); ?> id="<?php echo $this->get_field_id( 'facebook_display' ); ?>" name="<?php echo $this->get_field_name( 'facebook_display' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'facebook_display' ); ?>"><?php _e('Activate to Show Fanpage Total Count', 'social master'); ?></label></br>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'facebook_page' ); ?>"><?php _e('insert Facebook Page link (if above Total Count is selected):', 'social master'); ?></label></br>
+	<input id="<?php echo $this->get_field_id( 'facebook_page' ); ?>" name="<?php echo $this->get_field_name( 'facebook_page' ); ?>" value="<?php echo $instance['facebook_page']; ?>" style="width:auto;" />
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'facebook_id' ); ?>"><?php _e('Facebook Application ID (optional, leave blank):', 'social master'); ?></label></br>
+	<input id="<?php echo $this->get_field_id( 'facebook_id' ); ?>" name="<?php echo $this->get_field_name( 'facebook_id' ); ?>" value="<?php echo $instance['facebook_id']; ?>" style="width:auto;" />
+	</p>
+	<hr>
+	<p>
+	<input type="checkbox" <?php checked( (bool) $instance['show_twitter'], true ); ?> id="<?php echo $this->get_field_id( 'show_twitter' ); ?>" name="<?php echo $this->get_field_name( 'show_twitter' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_twitter' ); ?>"><b><?php _e('Twitter Button', 'social master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'twitter_user' ); ?>"><?php _e('Twitter Username:', 'social master'); ?></label></br>
+	<input id="<?php echo $this->get_field_id( 'twitter_user' ); ?>" name="<?php echo $this->get_field_name( 'twitter_user' ); ?>" value="<?php echo $instance['twitter_user']; ?>" style="width:auto;" />
+	</p>
+	<hr>
+	<p>
+	<input type="checkbox" <?php checked( (bool) $instance['show_google'], true ); ?> id="<?php echo $this->get_field_id( 'show_google' ); ?>" name="<?php echo $this->get_field_name( 'show_google' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_google' ); ?>"><b><?php _e('Google Button', 'social master'); ?></b></label>
+	</p>
+	<hr>
+	<p><b>Add all Social Networks to this widget.</b> Facebook Like, Twitter Follow and Re-Tweet, Google + plus, LinkedIn Share, Pinterest "pin it" Share, View on Instagram, Youtube Subscribe, Soundcloud Connect, StumbleUpon Share, MySpace Share, Digg Share, Reddit Share.</p>
+	<p><a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="More Social Networks">Add More Social Networks</a></p>
+	<?php
+	}
+ }
+?>

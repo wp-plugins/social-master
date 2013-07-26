45b246187ef6d288583e39dc1174ca158c1d0fa1
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-26 23:56:34 +0000

    fixing typos
    git-svn-id: https://plugins.svn.wordpress.org/social-master/trunk@746926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- techgasp-socialmaster-invite.php
+++ techgasp-socialmaster-invite.php
@@ -4,17 +4,15 @@ function techgasp_socialmaster_notice() {
 	global $current_user;
 	global $pagenow;
 	$user_id = $current_user->ID;
-
 /* Check that the user hasn't already clicked to ignore the message */
 	if ( ! get_user_meta($user_id, 'techgasp_socialmaster_notice') ) {
 		parse_str($_SERVER['QUERY_STRING'], $params);
 		if ( $pagenow == 'plugins.php' ) {
-	        echo '<div class="updated">';
-        	printf (__('<p><b>Congratulations!</b> Lite Plugin Installation Complete.</p>'));
-		printf (__('<p>You have a top of the line <em>TechGasp Plugin</em>. Don\'t be shy if you need help, just issue a support ticket <a class="button" href="http://wordpress.techgasp.com/support/" target="_blank" title="Support">Technical Support</a> | <a class="button" href="http://wordpress.techgasp.com/category/support/" target="_blank" title="Documentation">Documentation</a></p>'));
-		printf (__('<p><b>It\'s really important for us if you could take a minute to give us a good rating and comment in Wordpress Plugin Directory</b> <a class="button-primary" href="http://wordpress.org/plugins/social-master/" target="_blank" title="Please Rate Us"><b>RATE US</b></a></p>'));
-		printf (__('<p><a href="%s">Hide Notice</a>', 'n2cpanel'), '?' . http_build_query(array_merge($params, array('techgasp_socialmaster_ignore'=>'0'))));
-	        echo "</div>";
+			echo '<div class="updated">';
+		printf (__('<p><b>Congratulations!</b> Lite Plugin Installation Complete.</p>'));
+		printf (__('<p>If you need help don\'t be shy, just issue a support ticket <a class="button" href="http://wordpress.techgasp.com/support/" target="_blank" title="Support">Technical Support</a> | <a class="button" href="http://wordpress.techgasp.com/category/support/" target="_blank" title="Documentation">Documentation</a> | <a class="button" href="%s">Hide Notice</a></p>'), '?techgasp_socialmaster_ignore=0');
+		printf (__('<p><b>IMPORTANT!!!</b> please take a minute to and give us a good rating in Wordpress Plugin Directory</b> <a class="button-primary" href="http://wordpress.org/plugins/social-master/" target="_blank" title="Please Rate Us"><b>RATE US</b></a></p>'));
+			echo "</div>";
 		}
 	}
 }
@@ -22,11 +20,11 @@ add_action( 'admin_notices', 'techgasp_socialmaster_notice' );
 
 function techgasp_socialmaster_ignore() {
     global $current_user;
-        $user_id = $current_user->ID;
-        /* If user clicks to ignore the notice, add that to their user meta */
-        if ( isset($_GET['techgasp_socialmaster_ignore']) && '0' == $_GET['techgasp_socialmaster_ignore'] ) {
-             add_user_meta($user_id, 'techgasp_socialmaster_notice', 'true', true);
-    }
+		$user_id = $current_user->ID;
+		/* If user clicks to ignore the notice, add that to their user meta */
+		if ( isset($_GET['techgasp_socialmaster_ignore']) && '0' == $_GET['techgasp_socialmaster_ignore'] ) {
+		add_user_meta($user_id, 'techgasp_socialmaster_notice', 'true', true);
+		}
 }
 add_action('admin_init', 'techgasp_socialmaster_ignore');
 ?>

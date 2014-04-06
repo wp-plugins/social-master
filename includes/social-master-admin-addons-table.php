811987bc7873fa9b70706ee5a49a0af889a2c32f
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-06 01:10:55 +0000

    version 4.3.6
    git-svn-id: https://plugins.svn.wordpress.org/social-master/tags/4.3.6@888160 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/social-master-admin-addons-table.php
@@ -0,0 +1,66 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class social_master_admin_addons_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'social_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'social_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Installed', 'social_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Get Add-ons" style="float:left;">Get Add-ons</a></p></th>
+			<th class="manage-column column-columnname" scope="col"></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Get Add-ons" style="float:right;">Get Add-ons</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-widget-basic.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Social Master Basic Fast Loading Widget</h3><p>This Widget was specially designed in html5 to be easy to use and deploy, all settings are on auto mode. Extremely fast page load times and small system trace.</p><p>Packed with <b>Facebook Like</b> and <b>Share</b>, <b>Follow Twitter</b>, <b>Tweet</b> and <b>Google Plus</b> Buttons. Remember you can always hide or change the widget title.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-yes.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-widget-advanced.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Social Master Advanced Widget</h3><p>The "Top of the Line" Advanced Widget that contains all Social Network Buttons. <b>Facebook Like</b> and <b>Share</b>, <b>Twitter Follow</b> and <b>Tweet</b>, <b>Google Plus</b>, <b>LinkedIn Share</b>, <b>Tumblr Follow</b>, <b>Pinterest "pin it"</b>, <b>View on Instagram</b>, <b>Youtube Subscribe</b>, <b>Soundcloud Profile</b>, <b>Reverbnation Profile</b>, <b>Spotify Profile</b>, <b>StumbleUpon Share</b>, <b>MySpace Share</b>, <b>Buffer Share</b>, <b>Digg Share</b> and <b>Reddit Share</b>. Bombastic!!!</p><p> Social Master is currently the only plugin where users can decide which social network buttons to show and their order, yes you can order the buttons according to your social network priorities</p><p>Makes <b>NO USE</b> of nasty Javascipt or Ajax.</p></td>
+
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-shortcode-in.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Individual Shortcode</h3><p>Social Master uses TechGasp Wordpress Framework. The <b>Individual Shortcode</b> allows you to have a different customized social buttons in each page or post. Easy to use it can be found when you edit a page or a post under the wordpress text editor. Once you have created your shortcode, Just insert the shortcode <b>[social-master]</b> anywhere inside that text.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-shortcode-un.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Universal Shortcode</h3><p>Social Master uses TechGasp Wordpress Framework. The <b>Universal Shortcode</b> allows you to have the same social buttons across different pages or posts. Easy to use it can be found right here under the Shortcodes menu. Once you have created your shortcode, Just insert the shortcode <b>[social-master-un]</b> anywhere inside the text of your pages or posts.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-updater.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Advanced Updater</h3><p>The Advanced Updater allows you to easily Update / Upgrade your Advanced Plugin. You can instantly update your plugin after we release a new version with more goodies without having to wait for the nightly native wordpress updater that runs every 24/48 hours. Get it fresh, get it fast.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('social_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-socialmaster-admin-addons-support.png', __FILE__); ?>" alt="<?php echo get_option('facebook_comments_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Award Winning Professional Support</h3><p>Need professional help deploying this plugin? TechGasp provides 24/7 award winning professional wordpress support for all advanced version costumers and replies to support tickets usually within minutes of being received. Support Us and we will Support You.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('facebook_comments_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}

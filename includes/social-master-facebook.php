69df4cc7894a13dd10c089bef62ca8ec6dabac68
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 06:39:47 +0000

    wordpress 4.1
    git-svn-id: https://plugins.svn.wordpress.org/social-master/tags/4.3.7@1052287 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/social-master-facebook.php
@@ -0,0 +1,64 @@
+<?php
+//Display Facebook
+get_post_custom();
+if ( $show_display == 'on' ){
+	if ( $facebook_display ){
+		echo '<div id="fb-like" style="overflow: hidden; width:130px; line-height:100%;">' .
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<div class="fb-like" data-href="'.$facebook_page.'" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div></div>';
+	}
+	else {
+		echo '<div id="fb-like" style="overflow: hidden; width:130px; line-height:100%;">' .
+			'<div id="fb-root"></div>' .
+			'<script>(function(d, s, id) {' .
+			'var js, fjs = d.getElementsByTagName(s)[0];' .
+			'if (d.getElementById(id)) return;' .
+			'js = d.createElement(s); js.id = id;' .
+			'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+			'fjs.parentNode.insertBefore(js, fjs);' .
+			'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+			'<div class="fb-like" data-href="'.$uricurrent.'" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div></div>';
+	}
+}
+else{
+	if ( $facebook_display ){
+		echo '<center>' .
+		'<div id="fb-like" style="overflow: hidden; width:49px;">' .
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<style>.fb-like span{overflow:visible !important; width:450px !important; margin-right:-375px;}</style>' .
+		'<div class="fb-like" data-href="'.$facebook_page.'" data-send="false" data-layout="box_count" data-width="450" data-show-faces="false"></div>' .
+		'</div>' .
+		'</center>';
+	}
+	else {
+		echo '<center>' . 
+			'<div id="fb-like" style="overflow: hidden; width:49px;"'.
+			'<div id="fb-root"></div>' .
+			'<script>(function(d, s, id) {' .
+			'var js, fjs = d.getElementsByTagName(s)[0];' .
+			'if (d.getElementById(id)) return;' .
+			'js = d.createElement(s); js.id = id;' .
+			'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+			'fjs.parentNode.insertBefore(js, fjs);' .
+			'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+			'<style>.fb-like span{overflow:visible !important; width:450px !important; margin-right:-375px;}</style>' .
+			'<div class="fb-like" data-href="'.$uricurrent.'" data-send="false" data-layout="box_count" data-width="450" data-show-faces="false"></div>' .
+			'</div>' .
+			'</center>';
+	}
+}
+?>

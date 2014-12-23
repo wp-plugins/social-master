eaf30fc2aa01a3c5a6756c61a5e3467c95967033
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 06:39:47 +0000

    wordpress 4.1
    git-svn-id: https://plugins.svn.wordpress.org/social-master/tags/4.3.7.3@1052287 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/social-master-facebook.php
@@ -0,0 +1,62 @@
+<?php
+//Display Facebook
+get_post_custom();
+if ( $show_display == 'on' ){
+	if ( $facebook_display ){
+		echo '<div id="fb-like" style="width:133px;">' .
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<div class="fb-like" data-href="'.$facebook_page.'" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div></div>';
+	}
+	else {
+		echo '<div id="fb-like" style="width:133px;">' .
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<div class="fb-like" data-href="'.$uricurrent.'" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div></div>';
+	}
+}
+else{
+	if ( $facebook_display ){
+	echo '<div style="margin: 0 auto; display:table;">' .
+		'<div id="fb-like" style="width:49px;">' .
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<div class="fb-like" data-href="'.$facebook_page.'" data-send="true" data-layout="box_count" data-width="450" data-show-faces="false"></div>' .
+		'</div>' .
+		'</div>';
+	}
+	else {
+	echo '<div style="margin: 0 auto; display:table;">' .
+		'<div id="fb-like" style="width:49px;">'.
+		'<div id="fb-root"></div>' .
+		'<script>(function(d, s, id) {' .
+		'var js, fjs = d.getElementsByTagName(s)[0];' .
+		'if (d.getElementById(id)) return;' .
+		'js = d.createElement(s); js.id = id;' .
+		'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId='.$facebook_id.'";' .
+		'fjs.parentNode.insertBefore(js, fjs);' .
+		'}(document, '.$socialspacer.'script'.$socialspacer.', '.$socialspacer.'facebook-jssdk'.$socialspacer.'));</script>' .
+		'<div class="fb-like" data-href="'.$uricurrent.'" data-send="true" data-layout="box_count" data-width="450" data-show-faces="false"></div>' .
+		'</div>' .
+		'</div>';
+	}
+}
+?>
---
+++ includes/social-master-google.php
@@ -0,0 +1,28 @@
+<?php
+get_post_custom();
+//Display Google Plus
+if ( $show_display == 'on' ){
+	echo '<div id="___plusone_0" style="width: 65px; !important">' .
+		'<div class="g-plusone" id="___plusone_0" style="width: 65px; !important" data-size="medium"></div>' .
+		'<script type="text/javascript">' .
+		'(function() {' .
+		'var po = document.createElement('.$socialspacer.'script'.$socialspacer.'); po.type = '.$socialspacer.'text/javascript'.$socialspacer.'; po.async = true;' .
+		'po.src = '.$socialspacer.'https://apis.google.com/js/plusone.js'.$socialspacer.';' .
+		'var s = document.getElementsByTagName('.$socialspacer.'script'.$socialspacer.')[0]; s.parentNode.insertBefore(po, s);' .
+		'})();' .
+		'</script>' .
+		'</div>';
+}
+else{
+	echo '<div style="margin: 0 auto; display:table;">' .
+		'<div class="g-plusone" data-size="tall"></div>' .
+		'<script type="text/javascript">' .
+		'(function() {' .
+		'var po = document.createElement('.$socialspacer.'script'.$socialspacer.'); po.type = '.$socialspacer.'text/javascript'.$socialspacer.'; po.async = true;' .
+		'po.src = '.$socialspacer.'https://apis.google.com/js/plusone.js'.$socialspacer.';' .
+		'var s = document.getElementsByTagName('.$socialspacer.'script'.$socialspacer.')[0]; s.parentNode.insertBefore(po, s);' .
+		'})();' .
+		'</script>' .
+		'</div>';
+}
+?>

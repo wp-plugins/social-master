e0839910830a3f83b7aabd379d3fd6692ffac244
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 23:16:21 +0000

    version 4.4.1.8
    git-svn-id: https://plugins.svn.wordpress.org/social-master/tags/4.4.1.8@1159232 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/social-master-admin-settings-wide.php
@@ -0,0 +1,57 @@
+<?php
+function menu_single_social_master_admin_settings_wide(){
+	if ( is_admin() )
+	add_submenu_page( 'social-master', 'Settings', 'Settings', 'manage_options', 'social-master-admin-settings-wide', 'social_master_admin_settings_wide' );
+	}
+
+function social_master_admin_settings_wide(){
+?>
+<div class="wrap">
+<div style="width:40px; vertical-align:middle; float:left;"><img src="<?php echo plugins_url('../images/techgasp-minilogo.png', __FILE__); ?>" alt="' . esc_attr__( 'TechGasp Plugins') . '" /><br /></div>
+<h2><b>&nbsp;Settings</b></h2>
+<?php
+if(!class_exists('social_master_admin_settings_wide')){
+	require_once( dirname( __FILE__ ) . '/social-master-admin-settings-wide-table.php');
+}
+//Prepare Table of elements
+$wp_list_table = new social_master_admin_settings_wide_table();
+//Table of elements
+$wp_list_table->display();
+
+?>
+</br>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+</br>
+<?php
+if(!class_exists('social_master_admin_settings_wide_table_options')){
+	require_once( dirname( __FILE__ ) . '/social-master-admin-settings-wide-table-options.php');
+}
+//Prepare Table of elements
+$wp_list_table = new social_master_admin_settings_wide_table_options();
+//Table of elements
+$wp_list_table->display();
+?>
+</br>
+<h2>IMPORTANT: Makes no use of Javascript or Ajax to keep your website fast and conflicts free</h2>
+
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+
+<br>
+
+<p>
+<a class="button-secondary" href="http://wordpress.techgasp.com" target="_blank" title="Visit Website">More TechGasp Plugins</a>
+<a class="button-secondary" href="http://wordpress.techgasp.com/support/" target="_blank" title="Facebook Page">TechGasp Support</a>
+<a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Visit Website"><?php echo get_option('social_master_name'); ?> Info</a>
+<a class="button-primary" href="http://wordpress.techgasp.com/social-master-documentation/" target="_blank" title="Visit Website"><?php echo get_option('social_master_name'); ?> Documentation</a>
+<a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Visit Website">Get Add-ons</a>
+</p>
+<?php
+}
+
+if( is_multisite() ) {
+add_action( 'admin_menu', 'menu_single_social_master_admin_settings_wide' );
+}
+else {
+add_action( 'admin_menu', 'menu_single_social_master_admin_settings_wide' );
+}
+?>
---
+++ includes/social-master-widget-basic.php
@@ -0,0 +1,173 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'social_master_widget_basic' );
+//Register Widget
+function social_master_widget_basic() {
+register_widget( 'social_master_widget_basic' );
+}
+
+class social_master_widget_basic extends WP_Widget {
+	function social_master_widget_basic() {
+	$widget_ops = array( 'classname' => 'Social Master Basic', 'description' => __('This Widget was specially designed in html5 to be easy to use and deploy, all settings are on auto mode. Extremely fast page load times and small system trace. ', 'social_master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'social_master_widget_basic' );
+	$this->WP_Widget( 'social_master_widget_basic', __('Social Master Basic', 'social_master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		//Our variables from the widget settings.
+		$social_title = isset( $instance['social_title'] ) ? $instance['social_title'] :false;
+		$social_title_new = isset( $instance['social_title_new'] ) ? $instance['social_title_new'] :false;
+		@$uribase = site_url( $path, $scheme );
+		@$uricurrent ="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		$socialspacer ="'";
+		$show_display = isset( $instance['show_display'] ) ? $instance['show_display'] :false;
+		$show_facebook = isset( $instance['show_facebook'] ) ? $instance['show_facebook'] :false;
+		$show_twitter = isset( $instance['show_twitter'] ) ? $instance['show_twitter'] :false;
+		$show_google = isset( $instance['show_google'] ) ? $instance['show_google'] :false;
+		echo $before_widget;
+		
+		// Display the widget title
+	if ( $social_title ){
+		if (empty ($social_title_new)){
+			if(is_multisite()){
+			$social_title_new = get_site_option('social_master_name');
+			}
+			else{
+			$social_title_new = get_option('social_master_name');
+			}
+		echo $before_title . $social_title_new . $after_title;
+		}
+		else{
+		echo $before_title . $social_title_new . $after_title;
+		}
+	}
+	else{
+	}
+//CSS FOR BUTTONS
+	//HORIZONTAL
+	if ( $show_display == 'on' ) {
+	echo '<div class="social-master-buttons" style="display:flex !important;">';
+	//Display Facebook
+	if ( $show_facebook ){
+		require( dirname( __FILE__ ) . '/social-master-facebook.php');
+	}
+	else{
+	}
+	//Display Twitter
+	if ( $show_twitter ){
+		require( dirname( __FILE__ ) . '/social-master-twitter.php');
+	}
+	else{
+	}
+	//Display Google Plus
+	if ( $show_google ){
+		require( dirname( __FILE__ ) . '/social-master-google.php');
+	}
+	else{
+	}
+	echo '</div>';
+	}
+	else{
+		if ( $show_facebook ){
+		require( dirname( __FILE__ ) . '/social-master-facebook.php');
+	}
+	else{
+	}
+	//Display Twitter
+	if ( $show_twitter ){
+		require( dirname( __FILE__ ) . '/social-master-twitter.php');
+	}
+	else{
+	}
+	//Display Google Plus
+	if ( $show_google ){
+		require( dirname( __FILE__ ) . '/social-master-google.php');
+	}
+	else{
+	}
+	}
+		echo $after_widget;
+	}
+	//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['social_title'] = strip_tags( $new_instance['social_title'] );
+		$instance['social_title_new'] = $new_instance['social_title_new'];
+		$instance['show_display'] = $new_instance['show_display'];
+		update_option('social_master_wb_show_display', $new_instance['show_display']);
+		$instance['show_facebook'] = $new_instance['show_facebook'];
+		$instance['show_twitter'] = $new_instance['show_twitter'];
+		$instance['show_google'] = $new_instance['show_google'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'social_title_new' => __('Social Master', 'social_master'), 'social_title' => true, 'social_title_new' => false, 'show_display' => false, 'show_facebook' => false, 'show_twitter' => false, 'show_google' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+	<br>
+	<b>Check the buttons to be displayed:</b>
+		<!--TITLE-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['social_title'], true ); ?> id="<?php echo $this->get_field_id( 'social_title' ); ?>" name="<?php echo $this->get_field_name( 'social_title' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'social_title' ); ?>"><b><?php _e('Display Widget Title', 'social_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'social_title_new' ); ?>"><?php _e('Change Title:', 'social_master'); ?></label>
+	<br>
+	<input id="<?php echo $this->get_field_id( 'social_title_new' ); ?>" name="<?php echo $this->get_field_name( 'social_title_new' ); ?>" value="<?php echo $instance['social_title_new']; ?>" style="width:auto;" />
+	</p>
+		<!--DISPLAY-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_display'], true ); ?> id="<?php echo $this->get_field_id( 'show_display' ); ?>" name="<?php echo $this->get_field_name( 'show_display' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_display' ); ?>"><b><?php _e('Activates Horizontal Display', 'social_master'); ?></b></label></br>
+	<div class="description">Unchecked display is Vertical.</div>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<!--FACEBOOK-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_facebook'], true ); ?> id="<?php echo $this->get_field_id( 'show_facebook' ); ?>" name="<?php echo $this->get_field_name( 'show_facebook' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_facebook' ); ?>"><b><?php _e('Facebook Button', 'social_master'); ?></b></label></br>
+	</p>
+	<p>
+	<div class="description">Check Plugin Settings Page for Facebook Options.</div>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<!--TWITTER-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_twitter'], true ); ?> id="<?php echo $this->get_field_id( 'show_twitter' ); ?>" name="<?php echo $this->get_field_name( 'show_twitter' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_twitter' ); ?>"><b><?php _e('Twitter Button', 'social_master'); ?></b></label></br>
+	</p>
+	<p>
+	<div class="description">Check Plugin Settings Page for Twitter Options.</div>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<!--GOOGLE-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_google'], true ); ?> id="<?php echo $this->get_field_id( 'show_google' ); ?>" name="<?php echo $this->get_field_name( 'show_google' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_google' ); ?>"><b><?php _e('Google Button', 'social_master'); ?></b></label>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<!--NETWORKS-->
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Social Master Website</b>
+	</p>
+	<p><a class="button-secondary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Social Master Info Page">Info Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/social-master-documentation/" target="_blank" title="Social Master Documentation">Documentation</a> <a class="button-primary" href="http://wordpress.techgasp.com/social-master/" target="_blank" title="Get Add-ons">Get Add-ons</a></p>
+	<?php
+	}
+ }
+?>
